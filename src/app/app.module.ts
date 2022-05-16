a4b221626495136849bd2897d7f997855b390bd5
Natali27M <vns122716@gmail.com>
2022-05-16 09:14:14 +0300

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CommentComponent } from './components/comment/comment.component';
 import { CommentsComponent } from './components/comments/comments.component';
 import { PostComponent } from './components/post/post.component';
 import { PostsComponent } from './components/posts/posts.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { PostsComponent } from './components/posts/posts.component';
   ],
 
   imports: [
+    CommonModule,
     BrowserModule,
     HttpClientModule
   ],
